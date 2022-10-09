c80cb9408f91ed5475f41487484a5de201865946
Jnaneswar <nanimca000@gmail.com>
2022-10-09 11:46:23 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/" , methods=["GET","POST"])
 def index():
-    return "Starting Machine Learning Project"
+    return "Ci/CD pipeline has been created"
 
 if(__name__=="__main__"):
     app.run()
